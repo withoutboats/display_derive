c4be1b6d060edc39ade22bb4a8ad443a65e74b30
Bastien Orivel <eijebong@bananium.fr>
2018-03-03 03:18:57 +0100

    Bump syn to 0.12, quote to 0.4 and synstructure to 0.7

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate proc_macro;
-extern crate syn;
 
+#[macro_use] extern crate syn;
 #[macro_use] extern crate synstructure;
 #[macro_use] extern crate quote;
 
@@ -9,7 +9,7 @@ decl_derive!([Display, attributes(display)] => display_derive);
 fn display_derive(s: synstructure::Structure) -> quote::Tokens {
     #[cfg(feature = "std")]
     let display = display_body(&s).map(|display_body| {
-        s.bound_impl("::std::fmt::Display", quote! {
+        s.bound_impl(quote!(::std::fmt::Display), quote! {
             #[allow(unreachable_code)]
             fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
                 match *self { #display_body }
@@ -20,7 +20,7 @@ fn display_derive(s: synstructure::Structure) -> quote::Tokens {
 
     #[cfg(not(feature = "std"))]
     let display = display_body(&s).map(|display_body| {
-        s.bound_impl("::core::fmt::Display", quote! {
+        s.bound_impl(quote!(::core::fmt::Display), quote! {
             #[allow(unreachable_code)]
             fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
                 match *self { #display_body }
@@ -45,17 +45,17 @@ fn display_body(s: &synstructure::Structure) -> Option<quote::Tokens> {
         }
 
         let s = match msg[0] {
-            syn::NestedMetaItem::MetaItem(syn::MetaItem::NameValue(ref i, ref lit)) if i == "fmt" => {
-                lit.clone()
+            syn::NestedMeta::Meta(syn::Meta::NameValue(ref v)) if v.ident.as_ref() == "fmt" => {
+                v.lit.clone()
             }
             _ => panic!("Display attribute must begin `fmt = \"\"` to control the Display message."),
         };
         let args = msg[1..].iter().map(|arg| match *arg {
-            syn::NestedMetaItem::Literal(syn::Lit::Int(i, _)) => {
-                let bi = &v.bindings()[i as usize];
+            syn::NestedMeta::Literal(syn::Lit::Int(ref i)) => {
+                let bi = &v.bindings()[i.value() as usize];
                 quote!(#bi)
             }
-            syn::NestedMetaItem::MetaItem(syn::MetaItem::Word(ref id)) => {
+            syn::NestedMeta::Meta(syn::Meta::Word(ref id)) => {
                 if id.as_ref().starts_with("_") {
                     if let Ok(idx) = id.as_ref()[1..].parse::<usize>() {
                         let bi = &v.bindings()[idx];
@@ -78,15 +78,15 @@ fn display_body(s: &synstructure::Structure) -> Option<quote::Tokens> {
     }))
 }
 
-fn find_display_msg(attrs: &[syn::Attribute]) -> Option<&[syn::NestedMetaItem]> {
+fn find_display_msg(attrs: &[syn::Attribute]) -> Option<Vec<syn::NestedMeta>> {
     let mut display_msg = None;
     for attr in attrs {
-        if attr.name() == "display" {
+        if attr.path == parse_quote!(display) {
             if display_msg.is_some() {
                 panic!("Cannot have two display attributes")
             } else {
-                if let syn::MetaItem::List(_, ref list)  = attr.value {
-                    display_msg = Some(&list[..]);
+                if let Some(syn::Meta::List(ref list))  = attr.interpret_meta() {
+                    display_msg = Some(list.nested.iter().cloned().collect::<Vec<syn::NestedMeta>>());
                 } else {
                     panic!("Display attribute must take a list in parentheses")
                 }
